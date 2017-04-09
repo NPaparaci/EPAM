6f0df341c8891a84d62da515761d64b77883e33b
NPaparaci <N_paparaci@mail.ru>
2017-04-09 17:45:56 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Class_Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+   static class Class_Main
+    {
+        static public void Input(ref int[] arr, int l)
+        {
+            for (int i = 0; i < l; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                int[] arr;
+                arr = new int[6];
+
+            Class_Main.Input(ref arr,6);
+         
+
+
+            for (int i = 0; i < 6; i++)
+            {
+               
+                Console.WriteLine(arr[i]);
+                Console.ReadLine();
+            }
+
+
+        }
+        }
+}
